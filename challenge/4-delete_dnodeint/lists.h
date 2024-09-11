418a7ffcbbe1bd44a166cb63bca8cc98dc315021
Inès Oubabas <143883134+Ines-Oubabas@users.noreply.github.com>
2024-09-11 15:33:48 +0200

    Update lists.h

--- challenge/4-delete_dnodeint/lists.h
+++ challenge/4-delete_dnodeint/lists.h
@@ -6,16 +6,17 @@
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
- * @prev: points to the previous node
+ * @prev: points to the prev node
  * @next: points to the next node
  *
  * Description: doubly linked list node structure
+ * for Holberton project
  */
 typedef struct dlistint_s
 {
-    int n;
-    struct dlistint_s *prev;
-    struct dlistint_s *next;
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
